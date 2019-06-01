eb8b615034133d60d6d81c064315ba028c7f9830
Sarvesh Jain <sarvesh@ost.com>
2019-06-01 16:43:57 +0530

    Updates

---
+++ precache-manifest.3103753c68e1439e9f6bcf2ffbd7a630.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3ecd714e107c5c7528e549e0a0442f6",
+    "url": "/faucet-ui/index.html"
+  },
+  {
+    "revision": "3e152c87ec5b1dbd7492",
+    "url": "/faucet-ui/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "5b64058dca29f71f6e6a",
+    "url": "/faucet-ui/static/css/main.8ef63b32.chunk.css"
+  },
+  {
+    "revision": "3e152c87ec5b1dbd7492",
+    "url": "/faucet-ui/static/js/2.e82d5c13.chunk.js"
+  },
+  {
+    "revision": "5b64058dca29f71f6e6a",
+    "url": "/faucet-ui/static/js/main.8248bac8.chunk.js"
+  },
+  {
+    "revision": "62bee1a62391ca870fb4",
+    "url": "/faucet-ui/static/js/runtime~main.9b230983.js"
+  }
+]);
